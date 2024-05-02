ac653704a575bcc0c285e8b12d230719832c86bb
tvt-c0d3r <thanhtrinh97.info@gmail.com>
2024-05-03 00:16:04 +0700

    add all

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import SideBar from './components/Sidebar/SideBar.vue';
+  import Header from './components/Header/Header.vue';
+</script>
+
+<template>
+    <header id="header" class="navbar navbar-expand-lg navbar-fixed navbar-height navbar-flush navbar-container navbar-bordered">
+      <Header />
+    </header>
+
+  <main>
+  </main>
+</template>
+
+
